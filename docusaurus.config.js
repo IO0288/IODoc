f6720772f345ecdd4c7c7dec2ec56f5290281f9c
IO0288 <admin@io0288.cn>
2022-03-14 16:49:36 +0800

    feat: footer:Copyright
    Signed-off-by: IO0288 <admin@io0288.cn>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -178,7 +178,7 @@ const config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} <a href="https://blog.io0288.cn" class="footer__link-item">IO0288</a>, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: lightCodeTheme,
